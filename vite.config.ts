be4ecf3a30d3ab04c626292d44d07bc212bfa208
FelipeM2121 <f.moena.c@gmail.com>
2026-04-20 12:00:34 -0400

    Fix: Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/felipe-moena-portfolio/',
+  base: '/',
   server: {
     port: 3000,
   },
